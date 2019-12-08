cb5dc92e2cebecfc78a89c0ae9a2864c91dfa122
JonathanEquinox <58608916+JonathanEquinox@users.noreply.github.com>
2019-12-08 18:58:25 +0100

    Testing higher memory
    Actually I have no idea what this does just trying it out lmao

--- story/story_manager.py
+++ story/story_manager.py
@@ -28,7 +28,7 @@ class Story():
         if game_state is None:
             game_state = dict()
         self.game_state = game_state
-        self.memory = 20
+        self.memory = 40
 
     def __del__(self):
         if self.upload_story:
